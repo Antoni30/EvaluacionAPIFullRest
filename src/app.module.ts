8b5aa7843be187250cd3dcac9df6f5aec6cdda6a
Cristian Tello <cristian.andres.t98@gmail.com>
2024-03-07 21:22:53 -0500

    Tarea CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AulaModule } from './aula/aula.module';
+import { TareaModule } from './tarea/tarea.module';
 
 
 @Module({
-  imports: [AuthModule,MongooseModule.forRoot('mongodb+srv://toni:123Camila.web@web.mlxviwm.mongodb.net/?retryWrites=true&w=majority'), AulaModule],
+  imports: [AuthModule,MongooseModule.forRoot('mongodb+srv://toni:123Camila.web@web.mlxviwm.mongodb.net/?retryWrites=true&w=majority'), AulaModule, TareaModule],
   controllers: [AppController],
   providers: [AppService],
 })
